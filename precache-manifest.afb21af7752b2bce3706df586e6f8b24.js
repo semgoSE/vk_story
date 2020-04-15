bbf1c342be005fe88114197eeed0d4cf5d3341e5
semgoSE <sem1234d@gmail.com>
2020-04-15 21:27:44 +0300

    Updates

---
+++ precache-manifest.afb21af7752b2bce3706df586e6f8b24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5caecb9d95aad1f872804eb58978ff37",
+    "url": "/vk_story/index.html"
+  },
+  {
+    "revision": "f2cb2d6f095a1deafa53",
+    "url": "/vk_story/static/css/2.9e627a5a.chunk.css"
+  },
+  {
+    "revision": "f2cb2d6f095a1deafa53",
+    "url": "/vk_story/static/js/2.01a62aca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vk_story/static/js/2.01a62aca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abb279e37727b9b275e2",
+    "url": "/vk_story/static/js/main.6c4194d7.chunk.js"
+  },
+  {
+    "revision": "85e63006b4fdc9c68a35",
+    "url": "/vk_story/static/js/runtime-main.f77ef433.js"
+  }
+]);
