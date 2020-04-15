5997500cdc9691440ae42f593b42747248ae476b
semgoSE <sem1234d@gmail.com>
2020-04-15 23:23:10 +0300

    Updates

--- precache-manifest.963646da59b3243cf2197bc523b62353.js
+++ precache-manifest.963646da59b3243cf2197bc523b62353.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5abb42895a07cae61c37c6835efc23d9",
+    "revision": "7630bee244dd94f60049bec9e7170ea3",
     "url": "/vk_story/index.html"
   },
   {
-    "revision": "f2cb2d6f095a1deafa53",
+    "revision": "d7828876bdb9e29162b3",
     "url": "/vk_story/static/css/2.9e627a5a.chunk.css"
   },
   {
-    "revision": "f2cb2d6f095a1deafa53",
-    "url": "/vk_story/static/js/2.01a62aca.chunk.js"
+    "revision": "d7828876bdb9e29162b3",
+    "url": "/vk_story/static/js/2.84e95232.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/vk_story/static/js/2.01a62aca.chunk.js.LICENSE.txt"
+    "url": "/vk_story/static/js/2.84e95232.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "52a169fb288382cb6997",
-    "url": "/vk_story/static/js/main.80bf7b46.chunk.js"
+    "revision": "cba3ead7aab6206044bc",
+    "url": "/vk_story/static/js/main.5fc146f9.chunk.js"
   },
   {
     "revision": "85e63006b4fdc9c68a35",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vk_story/precache-manifest.5c1be3ab7482c8509dee2d08bb92a01a.js"
+  "/vk_story/precache-manifest.963646da59b3243cf2197bc523b62353.js"
 );
 
 self.addEventListener('message', (event) => {
